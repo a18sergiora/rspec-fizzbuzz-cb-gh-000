8132330cc17b36a56790264f4d0f31395db2984c
Sergio Romeu Agraso <a18sergiora@iessanclemente.net>
2019-08-06 11:30:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 require_relative 'spec/spec_helper.rb'
+
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 != 0
+    "Fizz"
+    puts "Fizz"
+    
